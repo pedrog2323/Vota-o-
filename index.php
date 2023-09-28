556efe96a42ee04a8e1c933c3dc1f17da22b7f36
pedrog2323 <130668535+pedrog2323@users.noreply.github.com>
2023-09-28 13:30:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,16 @@
     <title>Indicador de idade</title>
 </head>
 <body>
+    <!-- parte do envio da idade !-->
     <h1>Diga sua Idade</h1>
     <form method="post">
         <label for="idade">Digite a idade:</label>
         <input type="number" id="idade" name="idade" required>
         <button type="submit">Verificar</button>
     </form>
+    <!-- lógica -->
     <?php
+    // recebimento dos dados da caixa de números//
         $idade = $_POST["idade"];
             if($idade < 16 && $idade > 0){
                 echo "não pode votar";
@@ -24,6 +27,7 @@
             if($idade >= 66){
                 echo "voto facultativo";
             }
+            //resto 
             else {
                 echo "não é uma idade";
             }
